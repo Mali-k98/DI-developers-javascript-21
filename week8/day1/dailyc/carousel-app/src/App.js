9182cdf562cbab884bf74f700530a36a024e190f
Mali-k98 <amaliakemp@gmail.com>
2021-08-16 07:14:07 +0300

    'update'

---
+++ week8/day1/dailyc/carousel-app/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import Carousel from './Carousel';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <Carousel/>
+    </div>
+  );
+}
+
+export default App;
